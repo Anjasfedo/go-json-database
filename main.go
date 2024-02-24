fbc46c54395e6da0e1f770424fde91349566fd36
Anjasfedo <fedoaf98@gmail.com>
2024-02-24 23:45:32 +0700

    Define User Struct

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
-	"encoding/json"
 )
+
+type Address struct {
+	City    string
+	State   string
+	Country string
+	Pincode json.Number
+}
+
+type User struct {
+	Name    string
+	Age     json.Number
+	Contact string
+	Company string
+	Address Address
+}
 
 func main() {
 
