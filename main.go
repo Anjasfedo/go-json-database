c817ad9759ad71c683a92b359334ea716aa43160
Anjasfedo <fedoaf98@gmail.com>
2024-02-24 23:56:34 +0700

    Employees Slice

--- main.go
+++ main.go
@@ -22,5 +22,21 @@ type User struct {
 }
 
 func main() {
+	dir := "./"
 
+	db, err := New(dir, nil)
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+
+	employees := []User{
+		{"John", "23", "12121212", "Apel", Address{"bengkulu", "bengkulu", "indonesia", "1212"}},
+		{"Alice", "25", "13131313", "Jeruk", Address{"jakarta", "jakarta", "indonesia", "1313"}},
+		{"Michael", "30", "14141414", "Anggur", Address{"bandung", "jawa barat", "indonesia", "1414"}},
+		{"Samantha", "28", "15151515", "Pisang", Address{"surabaya", "jawa timur", "indonesia", "1515"}},
+		{"Emily", "26", "16161616", "Mangga", Address{"medan", "sumatera utara", "indonesia", "1616"}},
+		{"Daniel", "32", "17171717", "Strawberry", Address{"makassar", "sulawesi selatan", "indonesia", "1717"}},
+		{"Olivia", "27", "18181818", "Nanas", Address{"pontianak", "kalimantan barat", "indonesia", "1818"}},
+	}
+	
 }
